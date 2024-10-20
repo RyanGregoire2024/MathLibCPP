b2ba578c13bb3033b6ce3175e2a5f15a32af8ca7
RyanPSN <ryangregoire286@gmail.com>
2024-10-20 19:53:37 -0400

    Added More Function sto Different Things in C++ functional Programming Types

--- AlgebraLib/dllmain.cpp
+++ AlgebraLib/dllmain.cpp
@@ -1,6 +1,12 @@
+#include <string>
 #include "pch.h"
 #include <time.h>
 #include <stdlib.h>
+#include <vector>
+#include <iostream>
+
+
+using namespace std;
 
 
 int AddDays(int a, int b, int c, int d) {
@@ -20,6 +26,49 @@ int AddYears(int a) {
 int SubYears(int a) {
     return a - 365;
 }
+
+string getWords() {
+
+    srand(time(0));
+
+    string op[] = { "+", "-", "*", "/" };
+    int a = rand() % 3 + 1;
+
+    return op[a];
+}
+
+void generateFruitNames(string a, string b) {
+
+    for (int i = 0; i < 1; i++) {
+
+        string fruitName[] = { a, b };
+
+        cout << fruitName[0];
+        cout << fruitName[1];
+    }
+
+}
+
+void generatesNames(string a, string b, string c, string d, string e) {
+
+    for (int i = 0; i < 1; i++) {
+
+        string names[] = { a, b, c, d, e };
+
+        cout << names[0];
+        cout << names[1];
+        cout << names[2];
+        cout << names[3];
+        cout << names[4];
+    }
+
+
+
+
+}
+
+
+
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
